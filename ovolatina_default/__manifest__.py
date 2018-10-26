331db33d4a58126927d8349f0fa5d8e9d9303d29
jobiols <jorge.obiols@gmail.com>
2018-10-25 21:05:37 -0300

    [ADD] valerza

---
+++ ovolatina_default/__manifest__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+#
+#    Copyright (C) 2018  jeo Software  (http://www.jeosoft.com.ar)
+#    All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# -----------------------------------------------------------------------------
+{
+    'name': 'valerza',
+    'version': '12.0.0.0.0',
+    'license': 'Other OSI approved licence',
+    'category': 'Tools',
+    'summary': 'Customizacion para Valerza',
+    'author': 'jeo Software',
+    'depends': [
+
+        # para la localizacion argentina
+    ],
+    'data': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'images': [],
+
+    'port': '8069',
+    'repos': [
+        {'usr': 'jobiols', 'repo': 'cl-valerza', 'branch': '12.0'},
+
+    ],
+    'docker': [
+        {'name': 'odoo', 'usr': 'jobiols', 'img': 'odoo-jeo', 'ver': '12.0'},
+        {'name': 'postgres', 'usr': 'postgres', 'ver': '9.6'},
+        {'name': 'nginx', 'usr': 'nginx', 'ver': 'latest'},
+        {'name': 'aeroo', 'usr': 'adhoc', 'img': 'aeroo-docs'},
+    ]
+}
